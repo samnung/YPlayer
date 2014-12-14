78cfccfe872a7f9c91730675f170772bb8148025
Roman Kříž <samnung@gmail.com>
2014-12-14 22:06:57 +0100

    add HCYoutubeParser, implement simple logic to run custom video

--- YPlayer/ViewController.m
+++ YPlayer/ViewController.m
@@ -9,12 +9,16 @@
 
 #import "ViewController.h"
 #import "PlayerView.h"
+#import "HCYoutubeParser.h"
 
 
 
 @interface ViewController ()
 
-@property (weak) IBOutlet PlayerView *someView;
+@property (weak) IBOutlet NSTextField *inputTextField;
+@property (weak) IBOutlet NSButton *goButton;
+
+@property (weak) IBOutlet PlayerView *playerView;
 
 @end
 
@@ -26,11 +30,28 @@
 {
 	[super viewDidLoad];
 
-	self.someView.wantsLayer = YES;
-	self.someView.layer.backgroundColor = [NSColor grayColor].CGColor;
-	[self.someView setStreamURL:[NSURL URLWithString:@"https://archive.org/download/Pbtestfilemp4videotestmp4/video_test.mp4"]];
+	self.playerView.wantsLayer = YES;
+	self.playerView.layer.backgroundColor = [NSColor grayColor].CGColor;
+}
 
-	NSLog(@"viewDidLoad");
+- (void) viewWillDisappear
+{
+	[self.playerView stop];
+}
+
+
+#pragma mark
+
+- (IBAction) goButtonAction:(NSButton *)sender
+{
+	[HCYoutubeParser h264videosWithYoutubeURL:[NSURL URLWithString:self.inputTextField.stringValue]
+								completeBlock:^ (NSDictionary * videoDictionary, NSError * error) {
+									NSLog(@"error = %@", error);
+									NSLog(@"dict = %@", videoDictionary);
+
+									NSURL * streamURL = [NSURL URLWithString:videoDictionary[@"medium"]];
+									[self.playerView setStreamURL:streamURL];
+								}];
 }
 
 @end
